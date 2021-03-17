291a174bdcec1a1d9dc779822a222694336da76c
Estevão Rada <rada@dr.com>
2021-03-17 19:37:38 -0300

    comandos bd e try catch
    comandos dql e dml no banco, mas sem padronização ainda

--- RestauranteSenac/WinFuncListar.Designer.cs
+++ RestauranteSenac/WinFuncListar.Designer.cs
@@ -30,14 +30,28 @@
         {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.dgvFuncionarios = new System.Windows.Forms.DataGridView();
+            this.gpxCadastro = new System.Windows.Forms.GroupBox();
+            this.gpxEditar = new System.Windows.Forms.GroupBox();
+            this.lblNomeCad = new System.Windows.Forms.Label();
+            this.txbNomeCad = new System.Windows.Forms.TextBox();
+            this.txbEmailCad = new System.Windows.Forms.TextBox();
+            this.lblEmailCad = new System.Windows.Forms.Label();
+            this.lblTelCad = new System.Windows.Forms.Label();
+            this.txbTelCad = new System.Windows.Forms.MaskedTextBox();
+            this.lblSetorCad = new System.Windows.Forms.Label();
+            this.txbSetorCad = new System.Windows.Forms.TextBox();
+            this.txbFuncaoCad = new System.Windows.Forms.TextBox();
+            this.lblFuncaoCad = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).BeginInit();
+            this.gpxCadastro.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(214, 9);
+            this.lblTitulo.Location = new System.Drawing.Point(284, 21);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(213, 25);
             this.lblTitulo.TabIndex = 0;
@@ -46,23 +60,152 @@
             // 
             // dgvFuncionarios
             // 
+            this.dgvFuncionarios.AllowUserToAddRows = false;
+            this.dgvFuncionarios.AllowUserToResizeColumns = false;
+            this.dgvFuncionarios.AllowUserToResizeRows = false;
+            this.dgvFuncionarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvFuncionarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFuncionarios.Location = new System.Drawing.Point(235, 78);
+            this.dgvFuncionarios.Location = new System.Drawing.Point(22, 61);
             this.dgvFuncionarios.Name = "dgvFuncionarios";
-            this.dgvFuncionarios.Size = new System.Drawing.Size(369, 206);
+            this.dgvFuncionarios.ReadOnly = true;
+            this.dgvFuncionarios.Size = new System.Drawing.Size(718, 206);
             this.dgvFuncionarios.TabIndex = 1;
+            // 
+            // gpxCadastro
+            // 
+            this.gpxCadastro.Controls.Add(this.btnCadastrar);
+            this.gpxCadastro.Controls.Add(this.txbFuncaoCad);
+            this.gpxCadastro.Controls.Add(this.lblFuncaoCad);
+            this.gpxCadastro.Controls.Add(this.txbSetorCad);
+            this.gpxCadastro.Controls.Add(this.lblSetorCad);
+            this.gpxCadastro.Controls.Add(this.txbTelCad);
+            this.gpxCadastro.Controls.Add(this.lblTelCad);
+            this.gpxCadastro.Controls.Add(this.txbEmailCad);
+            this.gpxCadastro.Controls.Add(this.lblEmailCad);
+            this.gpxCadastro.Controls.Add(this.txbNomeCad);
+            this.gpxCadastro.Controls.Add(this.lblNomeCad);
+            this.gpxCadastro.Location = new System.Drawing.Point(22, 273);
+            this.gpxCadastro.Name = "gpxCadastro";
+            this.gpxCadastro.Size = new System.Drawing.Size(309, 198);
+            this.gpxCadastro.TabIndex = 2;
+            this.gpxCadastro.TabStop = false;
+            this.gpxCadastro.Text = "Cadastro";
+            // 
+            // gpxEditar
+            // 
+            this.gpxEditar.Location = new System.Drawing.Point(454, 273);
+            this.gpxEditar.Name = "gpxEditar";
+            this.gpxEditar.Size = new System.Drawing.Size(286, 198);
+            this.gpxEditar.TabIndex = 3;
+            this.gpxEditar.TabStop = false;
+            this.gpxEditar.Text = "Editar";
+            // 
+            // lblNomeCad
+            // 
+            this.lblNomeCad.AutoSize = true;
+            this.lblNomeCad.Location = new System.Drawing.Point(6, 25);
+            this.lblNomeCad.Name = "lblNomeCad";
+            this.lblNomeCad.Size = new System.Drawing.Size(38, 13);
+            this.lblNomeCad.TabIndex = 0;
+            this.lblNomeCad.Text = "Nome:";
+            // 
+            // txbNomeCad
+            // 
+            this.txbNomeCad.Location = new System.Drawing.Point(60, 22);
+            this.txbNomeCad.Name = "txbNomeCad";
+            this.txbNomeCad.Size = new System.Drawing.Size(233, 20);
+            this.txbNomeCad.TabIndex = 1;
+            // 
+            // txbEmailCad
+            // 
+            this.txbEmailCad.Location = new System.Drawing.Point(60, 48);
+            this.txbEmailCad.Name = "txbEmailCad";
+            this.txbEmailCad.Size = new System.Drawing.Size(233, 20);
+            this.txbEmailCad.TabIndex = 3;
+            // 
+            // lblEmailCad
+            // 
+            this.lblEmailCad.AutoSize = true;
+            this.lblEmailCad.Location = new System.Drawing.Point(6, 51);
+            this.lblEmailCad.Name = "lblEmailCad";
+            this.lblEmailCad.Size = new System.Drawing.Size(35, 13);
+            this.lblEmailCad.TabIndex = 2;
+            this.lblEmailCad.Text = "Email:";
+            // 
+            // lblTelCad
+            // 
+            this.lblTelCad.AutoSize = true;
+            this.lblTelCad.Location = new System.Drawing.Point(6, 77);
+            this.lblTelCad.Name = "lblTelCad";
+            this.lblTelCad.Size = new System.Drawing.Size(52, 13);
+            this.lblTelCad.TabIndex = 4;
+            this.lblTelCad.Text = "Telefone:";
+            // 
+            // txbTelCad
+            // 
+            this.txbTelCad.Location = new System.Drawing.Point(60, 74);
+            this.txbTelCad.Mask = "(99) 00000-0000";
+            this.txbTelCad.Name = "txbTelCad";
+            this.txbTelCad.Size = new System.Drawing.Size(233, 20);
+            this.txbTelCad.TabIndex = 5;
+            // 
+            // lblSetorCad
+            // 
+            this.lblSetorCad.AutoSize = true;
+            this.lblSetorCad.Location = new System.Drawing.Point(6, 103);
+            this.lblSetorCad.Name = "lblSetorCad";
+            this.lblSetorCad.Size = new System.Drawing.Size(35, 13);
+            this.lblSetorCad.TabIndex = 6;
+            this.lblSetorCad.Text = "Setor:";
+            // 
+            // txbSetorCad
+            // 
+            this.txbSetorCad.Location = new System.Drawing.Point(60, 100);
+            this.txbSetorCad.Name = "txbSetorCad";
+            this.txbSetorCad.Size = new System.Drawing.Size(233, 20);
+            this.txbSetorCad.TabIndex = 7;
+            // 
+            // txbFuncaoCad
+            // 
+            this.txbFuncaoCad.Location = new System.Drawing.Point(60, 126);
+            this.txbFuncaoCad.Name = "txbFuncaoCad";
+            this.txbFuncaoCad.Size = new System.Drawing.Size(233, 20);
+            this.txbFuncaoCad.TabIndex = 9;
+            // 
+            // lblFuncaoCad
+            // 
+            this.lblFuncaoCad.AutoSize = true;
+            this.lblFuncaoCad.Location = new System.Drawing.Point(6, 129);
+            this.lblFuncaoCad.Name = "lblFuncaoCad";
+            this.lblFuncaoCad.Size = new System.Drawing.Size(46, 13);
+            this.lblFuncaoCad.TabIndex = 8;
+            this.lblFuncaoCad.Text = "Função:";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(9, 152);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(284, 40);
+            this.btnCadastrar.TabIndex = 10;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // WinFuncListar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 372);
+            this.ClientSize = new System.Drawing.Size(772, 483);
+            this.Controls.Add(this.gpxEditar);
+            this.Controls.Add(this.gpxCadastro);
             this.Controls.Add(this.dgvFuncionarios);
             this.Controls.Add(this.lblTitulo);
             this.Name = "WinFuncListar";
             this.Text = "Funcionários :: Listar";
             this.Load += new System.EventHandler(this.WinFuncListar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).EndInit();
+            this.gpxCadastro.ResumeLayout(false);
+            this.gpxCadastro.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +215,18 @@
 
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.DataGridView dgvFuncionarios;
+        private System.Windows.Forms.GroupBox gpxCadastro;
+        private System.Windows.Forms.MaskedTextBox txbTelCad;
+        private System.Windows.Forms.Label lblTelCad;
+        private System.Windows.Forms.TextBox txbEmailCad;
+        private System.Windows.Forms.Label lblEmailCad;
+        private System.Windows.Forms.TextBox txbNomeCad;
+        private System.Windows.Forms.Label lblNomeCad;
+        private System.Windows.Forms.GroupBox gpxEditar;
+        private System.Windows.Forms.TextBox txbSetorCad;
+        private System.Windows.Forms.Label lblSetorCad;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txbFuncaoCad;
+        private System.Windows.Forms.Label lblFuncaoCad;
     }
 }
--- RestauranteSenac/WinFuncListar.cs
+++ RestauranteSenac/WinFuncListar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,17 +17,76 @@ namespace RestauranteSenac
         {
             InitializeComponent();
         }
-        //oq será executado quando a janela for exibida:
-        private void WinFuncListar_Load(object sender, EventArgs e)
+        private void atualizardados()
         {
             // Instanciar o classe de conexão com o bd:
             db.Banco objBanco = new db.Banco();
-
+            // Criar a "tabela" que será preenchida com os dados do BD:
+            DataTable tabela = new DataTable();
+            // Conectar com o banco:
+            objBanco.Conectar();
+            // Criar um objeto do tipo SQLiteCommand:
+            var cmd = objBanco.conexao.CreateCommand();
+            // Qual comando SQL será executado:
+            cmd.CommandText = "SELECT * FROM Funcionarios";
+            // Executar e obter os dados da consulta em um obj SQLiteDA:
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd.CommandText, objBanco.conexao);
+            // Preencher uma "tabela" com os dados retornados do BD:
+            da.Fill(tabela);
+            // Desconectar:
+            objBanco.Desconectar();
+            // Definir a fonte do DGV para a tabela que foi anteriormente preenchida:
+            dgvFuncionarios.DataSource = tabela;
+        }
+        //oq será executado quando a janela for exibida:
+        private void WinFuncListar_Load(object sender, EventArgs e)
+        {
+            atualizardados();
         }
 
         private void lblTitulo_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            // Instanciar e conectar ao banco:
+            db.Banco banco = new db.Banco();
+            try
+            {
+                banco.Conectar();
+                // Criar o objeto SQLiteCommand:
+                var cmd = banco.conexao.CreateCommand();
+                // Definir qual comando DML (Insert - Delete - Update) será executado:
+                cmd.CommandText = "INSERT INTO Funcionarios (Nome, Setor, Email, Telefone, Funcao) values (@nome, @setor, @email, @telefone, @funcao)";
+                // Definir a substituição dos parametros:
+                cmd.Parameters.AddWithValue("@nome", txbNomeCad.Text);
+                cmd.Parameters.AddWithValue("@setor", txbSetorCad.Text);
+                cmd.Parameters.AddWithValue("@email", txbEmailCad.Text);
+                cmd.Parameters.AddWithValue("@telefone", txbTelCad.Text);
+                cmd.Parameters.AddWithValue("@funcao", txbFuncaoCad.Text);
+                // Executar:
+                cmd.ExecuteNonQuery();
+                // Desconectar
+                banco.Desconectar();
+                // Mostrar mensagem confirmando o cadastro:
+                MessageBox.Show("Dados cadastrados com sucesso!");
+                // Atualizar os dados da tabela:
+                atualizardados();
+                // Limpar os campos do formulário:
+                txbNomeCad.Clear();
+                txbSetorCad.Clear();
+                txbEmailCad.Clear();
+                txbTelCad.Clear();
+                txbFuncaoCad.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Erro! Verifique os dados informados!");
+                // Desconectar
+                banco.Desconectar();
+            }
         }
     }
 }
